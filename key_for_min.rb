2a5440bc03abc767e74b2c2c31c688d344eab5d8
Andres Carvajal <jc1993box@hotmail.com>
2019-11-06 04:13:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -11,3 +11,14 @@ name_hash.each do |key, value|
     end 
   lowest_possible_key
 end 
+ 
+ smallest_key = nil
+  tiniest_value = nil
+  name_hash.each do |key, value|
+    if tiniest_value == nil || value < tiniest_value
+      tiniest_value = value
+      smallest_key = key
+      end
+    end
+    smallest_key
+end
